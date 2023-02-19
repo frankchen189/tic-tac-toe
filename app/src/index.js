0fc75f19da50c83d74039075b40ced7517512d5a
Frank Chen <yihui.chen@tigergraph.com>
2023-02-18 21:02:26 -0800

    support time travel;

--- app/src/index.js
+++ app/src/index.js
@@ -54,20 +54,14 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-
-        const winner = calculateWinner(current.squares);
-        if (winner) {
-            console.log('Winner: ' + winner);
-        } else {
-            console.log('Next player: ' + (this.state.xIsNext ? 'X' : 'O'));
-        }
 
         const squares = current.squares.slice();
         // Can't click a previously-clicked square.
@@ -86,15 +80,45 @@ class Game extends React.Component {
                 history: history.concat([{
                     squares: squares,
                 }]),
+                stepNumber: history.length,
                 xIsNext: !this.state.xIsNext,
             }
         );
 
     }
 
+    jumpTo(step) {
+        this.setState(
+            {
+                stepNumber: step,
+                xIsNext: (step % 2) === 0,
+            }
+        );
+    }
+
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
+
+        const winner = calculateWinner(current.squares);
+        if (winner) {
+            console.log('Winner: ' + winner);
+        } else {
+            console.log('Next player: ' + (this.state.xIsNext ? 'X' : 'O'));
+        }
 
         return (
             <div className="game">
@@ -102,6 +126,7 @@ class Game extends React.Component {
                     squares={current.squares}
                     onClick={(i) => this.handleClick(i)}
                 />
+                <ol>{moves}</ol>
             </div>
         );
 
